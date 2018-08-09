5c87a7890b936467d1ac791c76df95f88664c754
Yosia Hadisusanto <yosia.hadisusanto@gmail.com>
2018-08-09 13:55:19 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,9 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  
+  contacts.each do |name,  properties_hash|
+    properties_hash.each do |attribute, value|
+      if attribute == :favorite_ice_cream_flavors
+    end
+  end
 end
